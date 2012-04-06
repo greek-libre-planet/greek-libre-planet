fc1f940ab355385ab2ac69d54d524ead003bb05a
comzeradd <nikos@autoverse.net>
2012-04-06 12:13:15 +0300

    split themes

---
+++ themes/hellug/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+	// hide #back-top first
+	$("#back-top").hide();
+	
+	//image floating automation
+	if ($("img").width() > 400) {
+	    $("img").css("float", "none");
+	    $("img").addClass("bigimg");
+	}
+	
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 200) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+	
+	$('a#addform').click(function() {
+	    $('#editformbox').hide();
+	    $('#addformbox').toggle();
+	return false;
+    });
+	
+	$('a#editform').click(function() {
+	    $('#editformbox').toggle();
+	    $('#addformbox').hide();
+	return false;
+    });
+
+});
