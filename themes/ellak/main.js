457a24f59a0f18d4e55518264d35175721f6d7ca
Nikos Roussos <nikos@roussos.cc>
2013-10-09 11:34:14 +0300

    Update info and new submissions process

--- themes/ellak/main.js
+++ themes/ellak/main.js
@@ -1,43 +1,30 @@
 $(document).ready(function(){
 
-	// hide #back-top first
-	$("#back-top").hide();
-	
-	//image floating automation
-	if ($("img").width() > 400) {
-	    $("img").css("float", "none");
-	    $("img").addClass("bigimg");
-	}
-	
-	// fade in #back-top
-	$(function () {
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 200) {
-				$('#back-top').fadeIn();
-			} else {
-				$('#back-top').fadeOut();
-			}
-		});
+    // hide #back-top first
+    $("#back-top").hide();
 
-		// scroll body to 0px on click
-		$('#back-top a').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 800);
-			return false;
-		});
-	});
-	
-	$('a#addform').click(function() {
-	    $('#editformbox').hide();
-	    $('#addformbox').toggle();
-	return false;
-    });
-	
-	$('a#editform').click(function() {
-	    $('#editformbox').toggle();
-	    $('#addformbox').hide();
-	return false;
-    });
+    //image floating automation
+    if ($("img").width() > 400) {
+        $("img").css("float", "none");
+        $("img").addClass("bigimg");
+    }
 
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 200) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
 });
